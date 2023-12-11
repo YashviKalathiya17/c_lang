32f17c7a6615ed6cb179a786eb609d12dc3599bc
YashviKalathiya17 <147523363+YashviKalathiya17@users.noreply.github.com>
2023-12-11 20:16:33 +0530

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+using namespace std;
+main()
+{
+	char 'A';
+	cout<<"enter any string :";
+	char input_string[100];
+	cin.getline(input_tsring,size of(input-string));
+	for(int i=0;input_string[i]!='\0';i++) 
+	{
+		if(input_string[i]>='a'&&input_string[i]<='Z')
+		if(input_string[i]=input_string[i]-('a'-'A');
+		}
+		)
+	}	
+	printf("enter any charcater :");
+	scanf("%c",&'A');
+}
---
+++ 14.1.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<string.h>
+using namespace std;
+class empoloyee
+{
+	public:
+		
+		int id,age,salary,exoperience;
+		string emp_name,role,city,comapany_name;
+	};
+	class carRecordrySystem
+	{
+		int getint()
+		{
+			string getstring()
+			{
+				int main()
+	int main()
+	{
+		int i;
+		employee e1,e2,e3,e4,e5;
+		
+		cout<<"1) enter your name\t:";
+		e1.emp_name=getstring();
+		
+		cout<<"enter your id\t:";
+		e1.id=getint();
+		
+		cout<<"enter your age\t:";
+		e1.age=getint();
+		
+		cout<<"enter your role\t:";
+		e1.role=getstring();
+		
+		cout<<"enter your salary\t:";
+		e1.salary=getint();
+		
+		cout<<"enter your experience\t:";
+		e1.experience=getint();
+		
+		cout<<"1) your name is\t:"<<e1.emp_name<<end1
+				<<" your id is\t:"<<e1.id<<end1
+				<<" your age is\t:"<<e1.age<<end1
+				<<" your role is\t:"<<e1.role<<end1
+				<<" your salary is\t:"<<e1.salary<<end1
+				<<" your experience is\t:"<<e1.experience<<end1	
+					<<" your comapny name is\t:"<<e1.company name<<end1		}
+			}
+		}
+	}
+}
