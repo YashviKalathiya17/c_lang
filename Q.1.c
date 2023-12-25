28706d781fbfe4cdca0cdd98b5b028838398d5b6
YashviKalathiya17 <147523363+YashviKalathiya17@users.noreply.github.com>
2023-12-25 20:00:19 +0530

    Add files via upload

--- Q.1.c
+++ Q.1.c
@@ -1,17 +1,22 @@
 #include<stdio.h>
- voidmain()
+main()
 {
-void cube(int a)
-{
-	printf("enter a value of cube :");
-	scanf(%d\n",a*a*a);
+	int i,n;
+	printf("enter the size of the array :");
+	scanf("%d",&n);
+	int array[n];
+	for(i=0;i<n;i++)
+	{
+		printf("array[%d]: ",i);
+		scanf("%d",& array[i]);
+				}	
+
+	printf("negative elements from an array :");
+	for(i=0;i<n;i++)
+	{
+	if(array[i]<0)
+	{
+		printf("%d,",array[i]);
 	}
-		void main()
-		{
-			int a;
-			printf("enter value of a:");
-			scanf("%d",&a);
-			cube(a);
-			
-		}
+}
 }
